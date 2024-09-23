ceb3fac7ba77a466470f758225caf31b833b7a17
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-23 13:24:15 -0300

    Add files via upload

--- controladora/conexao.php
+++ controladora/conexao.php
@@ -10,6 +10,8 @@ $dbname = "ifhostgru_alcantaragas";
 //criação da conexão
 $conn = new mysqli ($servername, $username, $password, $dbname);
 
+print_r($conn);
+
 //verificando a conexão
 if (!$conn){
     die("conexão falhou" . mysqli_connect_error());
