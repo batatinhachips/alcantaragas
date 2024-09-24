d0c076cbd5d7b67130990137760e99c1487f3829
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-24 12:48:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,6 @@ include_once './modelo/produtos.php';
 include_once './repositorio/produtos_repositorio.php';
 include_once "./controladora/autenticacao.php";
 
-
-session_start();
-session_unset(); // Limpa todas as variáveis de sessão
-session_destroy(); // Destrói a sessão
-
 $produtosRepositorio = new produtoRepositorio($conn);
 $produtos = $produtosRepositorio->buscarTodos();
 ?>
