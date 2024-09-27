c2a70d255c498c58eda6fffc9ac1f6b6ad628860
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 17:30:09 -0300

    Update processar_exclusao.php

--- controladora/processar_exclusao.php
+++ controladora/processar_exclusao.php
@@ -2,6 +2,7 @@
 include '../controladora/conexao.php';
 include '../modelo/produtos.php';
 include '../repositorio/produtos_repositorio.php';
+include '../repositorio/usuarios_repositorio.php';
 
 $produtosRepositorio = new produtoRepositorio($conn);
 
@@ -16,5 +17,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
 
 // Redirecione para a página principal ou para onde desejar após a exclusão
 header('Location: ../visao/admin.php');
+
+$usuariosRepositorio = new usuarioRepositorio($conn);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+    $idParaExcluir = $_POST['id'];
+
+    // Excluir do banco de dados
+    $usuariosRepositorio->excluirUsuariosPorId($idParaExcluir);
+
+    header('Location: ../visao/admin_tabela.php');
+}
+
+// Redirecione para a página principal ou para onde desejar após a exclusão
 exit();
 ?>
