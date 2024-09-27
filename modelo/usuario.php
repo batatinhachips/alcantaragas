e0045e4cceb3bd6e3b2d4138b6629d3b8b55b147
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 17:12:45 -0300

    Add files via upload

--- modelo/usuario.php
+++ modelo/usuario.php
@@ -1,26 +1,52 @@
 <?php
 class Usuariosss {
     /* private $conn; //Sua conexão com o banco de dados */
-    private  $id_usuario;
-    private  $nome;
-    private  $email;
-    private  $senha;
-
-
-        public function __construct(
+    private $id_usuario;
+    private $nome;
+    private $email;
+    private $senha;
+    private $papel;
+    private $data_cadastro;
+    private $cpf;
+    private $telefone;
+    private $cep;
+    private $logradouro;
+    private $complemento;
+    private $numero;
+    private $bairro;
+    private $cidade;
+    
+    public function __construct(
         $id_usuario,
         $nome,
         $email,
         $senha,
-
-    )
-    {
+        $papel,
+        $data_cadastro,
+        $cpf,
+        $telefone,
+        $cep,
+        $logradouro,
+        $complemento,
+        $numero,
+        $bairro,
+        $cidade
+    ) {
         $this->id_usuario = $id_usuario;
         $this->nome = $nome;
         $this->email = $email;
         $this->senha = $senha;
-    }    
-
+        $this->papel = $papel;
+        $this->data_cadastro = $data_cadastro;
+        $this->cpf = $cpf;
+        $this->telefone = $telefone;
+        $this->cep = $cep;
+        $this->logradouro = $logradouro;
+        $this->complemento = $complemento;
+        $this->numero = $numero;
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+    }
 
     public function getIdUsuario()
     {
@@ -90,6 +116,176 @@ class Usuariosss {
     public function setSenha($senha)
     {
         $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function getPapel()
+    {
+        return $this->papel;
+    }
+
+    /**
+     * Set the value of preco
+     *
+     * @return  self
+     */ 
+    public function setPapel($papel)
+    {
+        $this->papel = $papel;
+
+        return $this;
+    }
+
+    public function getDataCadastro()
+    {
+        return $this->data_cadastro;
+    }
+
+    /**
+     * Set the value of data_cadastro
+     *
+     * @return  self
+     */ 
+    public function setDataCadastro($data_cadastro)
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Set the value of cpf
+     *
+     * @return  self
+     */ 
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set the value of telefone
+     *
+     * @return  self
+     */ 
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    /**
+     * Set the value of cep
+     *
+     * @return  self
+     */ 
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+
+    /**
+     * Set the value of logradouro
+     *
+     * @return  self
+     */ 
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    /**
+     * Set the value of complemento
+     *
+     * @return  self
+     */ 
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+
+        return $this;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     *
+     * @return  self
+     */ 
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * Set the value of bairro
+     *
+     * @return  self
+     */ 
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * Set the value of cidade
+     *
+     * @return  self
+     */ 
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
 
         return $this;
     }
