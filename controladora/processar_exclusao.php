8991c7f1145b522d4db1129df9776ac600b83d4a
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 17:50:55 -0300

    Add files via upload

--- controladora/processar_exclusao.php
+++ controladora/processar_exclusao.php
@@ -2,9 +2,8 @@
 include '../controladora/conexao.php';
 include '../modelo/produtos.php';
 include '../repositorio/produtos_repositorio.php';
-include '../repositorio/usuarios_repositorio.php';
 
-$produtosRepositorio = new produtoRepositorio($conn)
+$produtosRepositorio = new produtoRepositorio($conn);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $idParaExcluir = $_POST['id'];
@@ -12,21 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     // Excluir do banco de dados
     $produtosRepositorio->excluirProdutosPorId($idParaExcluir);
 
-   header('Location: ../visao/admin.php');
-}
-
-
-
-$usuariosRepositorio = new usuarioRepositorio($conn);
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_usuario'])) {
-    $idParaExcluir = $_POST['id_usuario'];
-
-    // Excluir do banco de dados
-    $usuariosRepositorio->excluirUsuariosPorId($idParaExcluir);
-
-    header('Location: ../visao/admin_tabela.php');
+    // Adicione aqui qualquer lógica adicional necessária para a exclusão no site, se aplicável
 }
 
 // Redirecione para a página principal ou para onde desejar após a exclusão
+header('Location: ../visao/admin.php');
 exit();
 ?>
