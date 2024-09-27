f523ffb83a68ceaa0cadc586fef9ced40a9e7135
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 09:12:18 -0300

    Add files via upload

--- controladora/processar_editar_admin.php
+++ controladora/processar_editar_admin.php
@@ -1,11 +1,11 @@
 <?php
 // Incluir o arquivo de conexão com o banco de dados
 include '../controladora/conexao.php';
-include '../modelo/login.php';
-include '../repositorio/admins_repositorio.php';
+include '../modelo/usuario.php';
+include '../repositorio/usuarios_repositorio.php';
 
 $usuariosRepositorio = new usuarioRepositorio($conn);
-$usuarios = $usuariosRepositorio->buscarTodos();
+$usuarios = $usuariosRepositorio->buscarTodosUsuarios();
 
 // Verificar se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,13 +13,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_usuario = $_POST["id_usuario"];
     $nome = $_POST["nome"];
     $email = $_POST["email"];
-    $papel = $_POST["papel"];
+    $senha = $_POST["senha"];
+    
+    // $papel = $_POST["papel"];
+
+
+
 
     // Atualizar as informações do produto no banco de dados
     $sql = "UPDATE usuario SET 
                 nome='$nome',
                 email='$email',
-                papel='$papel'
+                senha= '$senha'
+
+                -- papel='$papel'
+
+
             WHERE id=$id_usuario";
 
     if ($conn->query($sql) === TRUE) {
