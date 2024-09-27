7ed5bfb540f8e444a360a78721daba676ce0d049
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 17:55:04 -0300

    Update processar_exclusao.php

--- controladora/processar_exclusao.php
+++ controladora/processar_exclusao.php
@@ -2,19 +2,38 @@
 include '../controladora/conexao.php';
 include '../modelo/produtos.php';
 include '../repositorio/produtos_repositorio.php';
+include '../modelo/usuarios.php'; // Inclua o modelo de usuários
+include '../repositorio/usuarios_repositorio.php'; // Inclua o repositório de usuários
 
 $produtosRepositorio = new produtoRepositorio($conn);
+$usuariosRepositorio = new usuarioRepositorio($conn);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $idParaExcluir = $_POST['id'];
 
-    // Excluir do banco de dados
-    $produtosRepositorio->excluirProdutosPorId($idParaExcluir);
+    if (isset($_POST['tipo']) && $_POST['tipo'] === 'produto') {
+        // Excluir produto
+        $produtosRepositorio->excluirProdutosPorId($idParaExcluir);
 
-    // Adicione aqui qualquer lógica adicional necessária para a exclusão no site, se aplicável
+        // Redirecionar após exclusão de produto
+        header('Location: ../visao/admin.php');
+        exit();
+
+    } elseif (isset($_POST['tipo']) && $_POST['tipo'] === 'usuario') {
+        // Excluir usuário
+        $usuariosRepositorio->excluirUsuarioPorId($idParaExcluir);
+
+        // Verifique se o usuário é um administrador antes de redirecionar
+        $usuarioExcluido = $usuariosRepositorio->buscarUsuarioPorId($idParaExcluir);
+        if ($usuarioExcluido && $usuarioExcluido['tipo'] === 'admin') {
+            // Redireciona para a página tabela_admin se for um administrador
+            header('Location: ../visao/tabela_admin.php');
+            exit();
+        }
+
+        // Redirecionar para outra página caso seja um usuário comum (se necessário)
+        header('Location: ../visao/admin.php');
+        exit();
+    }
 }
-
-// Redirecione para a página principal ou para onde desejar após a exclusão
-header('Location: ../visao/admin.php');
-exit();
 ?>
