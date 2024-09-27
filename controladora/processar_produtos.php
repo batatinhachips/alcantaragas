9a594ab065797642eb884b0896f70bb5d9f12876
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 18:42:48 -0300

    Update processar_produtos.php

--- controladora/processar_produtos.php
+++ controladora/processar_produtos.php
@@ -4,13 +4,14 @@ require './conexao.php';
 require '../modelo/produtos.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Capturando os dados do formulário
     $nome = $_POST["nome"];
     $descricao = $_POST["descricao"];
     $preco = $_POST["preco"];
 
     // Verificar se um arquivo de imagem foi enviado
     if ($_FILES["imagem"]["error"] == 0) {
-        // Diretório onde você deseja armazenar as imagens (ajuste conforme necessário)
+        // Diretório onde você deseja armazenar as imagens
         $diretorio_destino = "../recursos/imagens/";
 
         // Nome do arquivo da imagem
@@ -21,11 +22,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Mover o arquivo para o diretório de destino
         if (move_uploaded_file($_FILES["imagem"]["tmp_name"], $caminho_completo)) {
-            $produto = new produto(0, $nome, $descricao, $preco,  $nome_arquivo);
+            // Instanciando o objeto produto com os dados corretos
+            $produto = new produto(0, $nome, $descricao, $preco, $nome_arquivo);
 
+            // Salvando no banco de dados
             $produtoRepositorio = new produtoRepositorio($conn);
             $produtoRepositorio->cadastrar($produto);
 
+            // Redirecionando para a página de sucesso
             header("Location: ../visao/cadastrar_produtos_sucesso.php");
         } else {
             echo "Erro ao fazer upload do arquivo.";
@@ -35,3 +39,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+
