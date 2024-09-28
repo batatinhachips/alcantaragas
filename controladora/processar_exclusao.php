6362d7d78b361f7e4c8c8b5aabd1750f5fb90bf6
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-28 17:30:35 -0300

    Update processar_exclusao.php

--- controladora/processar_exclusao.php
+++ controladora/processar_exclusao.php
@@ -2,10 +2,9 @@
 include '../controladora/conexao.php';
 include '../modelo/produtos.php';
 include '../repositorio/produtos_repositorio.php';
-include_once '../modelo/usuario.php';
+include '../modelo/usuario.php'; // Inclua o modelo de usuários
 include '../repositorio/usuarios_repositorio.php';
-include_once '../visao/admin_tabela.php';
-include_once '../visao/usuarios_tabela.php';
+include '../modelo/login.php';// Inclua o repositório de usuários
 
 $produtosRepositorio = new produtoRepositorio($conn);
 $usuariosRepositorio = new usuarioRepositorio($conn);
@@ -16,25 +15,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     if (isset($_POST['tipo']) && $_POST['tipo'] === 'produto') {
         // Excluir produto
         $produtosRepositorio->excluirProdutosPorId($idParaExcluir);
+
+        // Redirecionar após exclusão de produto
         header('Location: ../visao/admin.php');
         exit();
+
     } elseif (isset($_POST['tipo']) && $_POST['tipo'] === 'usuario') {
         // Excluir usuário
         $usuariosRepositorio->excluirUsuariosPorId($idParaExcluir);
 
-        // Redirecionar com base na página de origem
-        if (isset($_POST['pagina_origem'])) {
-            if ($_POST['pagina_origem'] === 'admin_tabela') {
-                header('Location: ../visao/admin_tabela.php');
-            } elseif ($_POST['pagina_origem'] === 'usuario_tabela') {
-                header('Location: ../visao/usuario_tabela.php');
-            }
-        } else {
-            // Redirecionamento padrão caso não seja identificada a origem
-            header('Location: ../visao/admin.php');
+        // Verifique se o usuário é um administrador antes de redirecionar
+       
+        header('Location: ../visao/admin_tabela.php');
+       
         }
-
         exit();
     }
-}
 ?>
