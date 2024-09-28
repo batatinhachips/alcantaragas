91a53546a7cb30e614c8785eda977c7c8f225eb9
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-28 17:13:19 -0300

    Update processar_exclusao.php

--- controladora/processar_exclusao.php
+++ controladora/processar_exclusao.php
@@ -4,7 +4,7 @@ include '../modelo/produtos.php';
 include '../repositorio/produtos_repositorio.php';
 include '../modelo/usuario.php'; // Inclua o modelo de usuários
 include '../repositorio/usuarios_repositorio.php';
-include '../modelo/login.php'// Inclua o repositório de usuários
+include '../modelo/login.php';// Inclua o repositório de usuários
 
 $produtosRepositorio = new produtoRepositorio($conn);
 $usuariosRepositorio = new usuarioRepositorio($conn);
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
         $usuariosRepositorio->excluirUsuariosPorId($idParaExcluir);
 
         // Verifique se o usuário é um administrador antes de redirecionar
-       elseif (isset($_POST['tipo']) && $_POST['tipo'] === 'usuario') {
+    }elseif (isset($_POST['tipo']) && $_POST['tipo'] === 'usuario') {
         // Excluir usuário
         $usuariosRepositorio->excluirUsuariosPorId($idParaExcluir);
 
@@ -38,6 +38,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
         }
         exit();
     }
-   } 
 }
+
 ?>
