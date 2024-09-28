7b2d85a8dcbd493b89d5df29f8fac1f72585ec52
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-27 22:44:11 -0300

    Update processar_editar_admin.php

--- controladora/processar_editar_admin.php
+++ controladora/processar_editar_admin.php
@@ -40,10 +40,10 @@ $usuarios = $usuariosRepositorio->buscarTodosAdmins();
 // Verificar se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obter os dados do formulário
-    $id_usuario = $_POST["id_usuario"] ?? null; // Mudei aqui para corresponder ao nome do índice
-    $nome = $_POST["nome"] ?? null;
-    $email = $_POST["email"] ?? null;
-    $senha = $_POST["senha"] ?? null;
+    $id_usuario = $_POST["id_usuario"]; // Mudei aqui para corresponder ao nome do índice
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
     // Atualizar as informações do produto no banco de dados
     $stmt = $conn->prepare("UPDATE usuario SET nome=?, email=?, senha=? WHERE id_usuario=?");
