4bd502dc05fb465917694713f2b2745ba789e11d
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-28 00:20:58 -0300

    Update editar_usuario.php

--- visao/editar_usuario.php
+++ visao/editar_usuario.php
@@ -22,6 +22,11 @@ session_start();
   <script src="../recursos/js/popper.min.js"></script>
   <script src="../recursos/js/script.js"></script>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 
   <!-- FIM DOS LINKS -->
 </head>
