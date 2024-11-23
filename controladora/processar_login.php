bbc01ecd82e36a7e6af400eedda9867a32ebe2ee
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-11-23 19:48:29 -0300

    Update processar_login.php

--- controladora/processar_login.php
+++ controladora/processar_login.php
@@ -2,9 +2,6 @@
 include "../controladora/autenticacao.php";
 include "../controladora/conexao.php";
 
-error_reporting(0);
-ini_set('display_errors',0);
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $senha = $_POST["senha"];
