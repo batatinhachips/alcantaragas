7b1e0b1b54cfa75e1b9d4b94e58bd424954f0746
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-11-23 20:07:02 -0300

    Update estoque.php

--- visao/estoque.php
+++ visao/estoque.php
@@ -1,7 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-ob_end_flush();
+
+echo $undefined_variable; 
 
 include '../controladora/conexao.php';
 include '../modelo/produtos.php';
