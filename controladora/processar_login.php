08268bf7f77462f9e1c0166c79fb8eb4e47e379a
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-11-23 19:45:49 -0300

    Update processar_login.php

--- controladora/processar_login.php
+++ controladora/processar_login.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION["usuario"] = $usuario["email"];
         $_SESSION["nome_usuario"] = $usuario["nome"];
         $_SESSION["idNivelUsuario"] = $usuario["idNivelUsuario"];
-        header("Location: ../visao/index.php");
+        header("Location: ../index.php");
         exit;
     } else {
         header("Location: ../visao/formLogin.php?erro=1");
