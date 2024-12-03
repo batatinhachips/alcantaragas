b2a4d13d0cd7f3dfaa2e3050cf992b4357da72c1
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-12-03 00:06:17 -0300

    Update admin.php

--- visao/admin.php
+++ visao/admin.php
@@ -5,11 +5,11 @@ session_start();
 ?>
 
 <head>
-  <title>Administração</title>
-
+  <title>ADMINISTRAÇÃO</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- LINKS -->
   <link rel="icon" href="../recursos/imagens/icon.png" type="image/png">
   <link href="../recursos/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
@@ -33,6 +33,7 @@ $produtos = $produtosRepositorio->buscarTodos();
 ?>
 
 <body>
+
   <nav class="navbar navbar-expand-sm navbar-custom navbar-dark fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">
@@ -42,7 +43,6 @@ $produtos = $produtosRepositorio->buscarTodos();
       <div class="menu-icon" onclick="toggleMenu()">
         <i class="bi bi-list"></i>
       </div>
-
       <nav id="menu" class="menu">
         <?php
         if (isset($_SESSION["nome_usuario"])) {
@@ -61,31 +61,29 @@ $produtos = $produtosRepositorio->buscarTodos();
           <?php } ?>
         </div>
       </nav>
-
     </div>
-
-    <div id="navbarNav" class="navbar-nav">
-      <ul class="navbar-navButton">
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="cadastrar_admin.php">Novo Admin</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="cadastrar_produtos.php">Novo Produto</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="admin_tabela.php">Tabela Admins</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="usuario_tabela.php">Tabela Clientes</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="pedidos.php">Tabela de Vendas</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-light ms-2" href="estoque.php">Estoque</a>
-        </li>
-      </ul>
-    </div>
+      <div id="navbarNav" class="navbar-nav">
+        <ul class="navbar-navButton">
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="cadastrar_admin.php">Novo Admin</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="cadastrar_produtos.php">Novo Produto</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="admin_tabela.php">Tabela Admins</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="usuario_tabela.php">Tabela Clientes</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="pedidos.php">Tabela de Vendas</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-light ms-2" href="estoque.php">Estoque</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 
@@ -102,15 +100,15 @@ $produtos = $produtosRepositorio->buscarTodos();
                 <p class="custom-card-text"><?= $produto->getDescricao() ?></p>
                 <h4>R$ <?= number_format($produto->getPrecoProduto(), 2, ',', '.') ?></h4>
                 <div class="botao-container">
-                  <form action="../visao/editar_produtos.php" method="POST">
-                    <input type="hidden" name="idProduto" value="<?= $produto->getIdProduto(); ?>">
-                    <input type="submit" class="botao-editar" value="Editar">
-                  </form>
+                <form action="../visao/editar_produtos.php" method="POST">
+                  <input type="hidden" name="idProduto" value="<?= $produto->getIdProduto(); ?>">
+                  <input type="submit" class="botao-editar" value="Editar">
+                </form>
 
-                  <button class="botao-excluir" data-id="<?= $produto->getIdProduto(); ?>" data-tipo="produto">
-                    Excluir
-                  </button>
-                </div>
+                <button class="botao-excluir" data-id="<?= $produto->getIdProduto(); ?>" data-tipo="produto">
+                  Excluir
+                </button>
+              </div>
               </div>
             </div>
           </div>
@@ -118,36 +116,33 @@ $produtos = $produtosRepositorio->buscarTodos();
       </div>
     </div>
   </section>
-
   <script>
-    $(document).on('click', '.botao-excluir', function() {
-      const idParaExcluir = $(this).data('id');
-      const tipo = $(this).data('tipo');
+  $(document).on('click', '.botao-excluir', function() {
+    const idParaExcluir = $(this).data('id');
+    const tipo = $(this).data('tipo');
 
-      $.ajax({
+    $.ajax({
         url: '../controladora/processar_exclusao.php',
         type: 'POST',
         dataType: 'json',
         data: {
-          id: idParaExcluir,
-          tipo: tipo
+            id: idParaExcluir,
+            tipo: tipo
         },
         success: function(response) {
-          if (response.status === 'sucesso') {
-            $(`#produto-${idParaExcluir}`).fadeOut(300, function() {
-              $(this).remove();
-            });
-          } else {
-            alert(response.message || 'Erro ao excluir.');
-          }
+            if (response.status === 'sucesso') {
+              $(`#produto-${idParaExcluir}`).fadeOut(300, function() {
+                    $(this).remove();
+                });
+            } else {
+                alert(response.message || 'Erro ao excluir.');
+            }
         },
         error: function() {
-          alert('Erro na solicitação. Tente novamente.');
+            alert('Erro na solicitação. Tente novamente.');
         }
-      });
     });
-  </script>
-
+}); </script>
 </body>
 
 </html>
