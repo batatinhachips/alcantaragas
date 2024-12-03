7e5ed7a8f5362fa5569331f3446b03843c750355
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-12-02 23:04:37 -0300

    Update usuario_tabela.php

--- visao/usuario_tabela.php
+++ visao/usuario_tabela.php
@@ -16,6 +16,10 @@ session_start();
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <script src="../recursos/js/bootstrap.bundle.min.js"></script>
+  <script src="../recursos/js/jquery-3.5.1.min.js"></script>
+  <script src="../recursos/js/popper.min.js"></script>
+  <script src="../recursos/js/script.js"></script>
 
 </head>
 <?php
@@ -157,11 +161,6 @@ $usuarios = $usuariosRepositorio->buscarTodosClientes();
       });
     });
   </script>
-
-  <script src="../recursos/js/bootstrap.bundle.min.js"></script>
-  <script src="../recursos/js/jquery-3.5.1.min.js"></script>
-  <script src="../recursos/js/popper.min.js"></script>
-  <script src="../recursos/js/script.js"></script>
 
 </body>
 
